7c563c8d66a63fdf672a08f0921164f5358a6716
Robert Marks <robert.marks54@gmail.com>
2020-07-11 22:23:55 -0600

    Updates

---
+++ precache-manifest.bca24aaaa3c58f4e852aa35739c4a8ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbdad8a7aceaa0e1a053855f3f05d97",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "dd82c0e243f92474f251",
+    "url": "/react-clicky-game/static/css/main.ee0479d8.chunk.css"
+  },
+  {
+    "revision": "37a83b81ae7ff613c9c4",
+    "url": "/react-clicky-game/static/js/2.2efd371a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-clicky-game/static/js/2.2efd371a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd82c0e243f92474f251",
+    "url": "/react-clicky-game/static/js/main.9a92b7dd.chunk.js"
+  },
+  {
+    "revision": "bcbffaf2cf849aa9bc8e",
+    "url": "/react-clicky-game/static/js/runtime-main.6f14b782.js"
+  }
+]);
